11ec79ed862ff815ad51e999fa5fab6f65fcfc06
Pekka Ekman <pekka.ekman@aalto.fi>
2013-11-12 17:52:27 +0200

    created new app

---
+++ gen/com/github/robinbj86/energywastingapp/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.github.robinbj86.energywastingapp;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
