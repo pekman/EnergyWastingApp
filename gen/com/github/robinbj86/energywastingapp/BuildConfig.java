2d392fcf816170d5eb648c3304b39be797cdd157
Pekka Ekman <pekka.ekman@aalto.fi>
2013-11-12 19:19:31 +0200

    added gen to .gitignore and removed bin and gen from repository

--- gen/com/github/robinbj86/energywastingapp/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.github.robinbj86.energywastingapp;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
