b92f1a18b9977b7c4cf2d23ec03c1e86be490aeb
robinbj86 <robin.jerome@outlook.com>
2013-12-07 13:36:36 +0200

    downloading file from url

--- src/com/github/robinbj86/energywastingapp/components/WiFiDataTransfer.java
+++ src/com/github/robinbj86/energywastingapp/components/WiFiDataTransfer.java
@@ -1,21 +1,30 @@
 package com.github.robinbj86.energywastingapp.components;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.HttpURLConnection;
 import java.net.InetAddress;
+import java.net.URL;
 import java.util.Random;
 
+import android.os.Environment;
 import android.util.Log;
 
 
 public class WiFiDataTransfer extends Component {
 
 	public static final String SERVERIP = "192.168.0.3"; 
+	private URL url;
+	public static final String urlString = "http://www.ndtv.com/sitemap.xml";
 	public static final int SERVERPORT = 4444; 
 	private static DatagramSocket socket = null;
 	private static InetAddress serverAddr = null;
 	private Thread sendingThread = null;
+	private Thread downloadThread = null;
 	private static boolean running = false;
 	
 	@Override
@@ -34,6 +43,10 @@ public class WiFiDataTransfer extends Component {
 			if(null == serverAddr){
 				serverAddr = InetAddress.getByName(SERVERIP); 
 			}
+			
+			if(null == url) {
+				url = new URL(urlString);
+			}
 
 			sendingThread = new Thread() {
 				@Override
@@ -41,8 +54,16 @@ public class WiFiDataTransfer extends Component {
 					sendUDPDataToAddress(serverAddr, socket);
 				};
 			};
+			
+			downloadThread = new Thread() {
+				@Override
+				public void run() {
+					downloadDataToSDCard();
+				};
+			};
 
 			sendingThread.start();
+			downloadThread.start();
 
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -82,5 +103,63 @@ public class WiFiDataTransfer extends Component {
 					e.printStackTrace();
 				} 
 		}
+	}
+	
+	private void downloadDataToSDCard() {
+		HttpURLConnection urlConnection = null;
+		File SDCardRoot = null;
+		File destinationFile = null;
+		FileOutputStream fileOutput = null;
+		InputStream inputStream = null;
+		while(WiFiDataTransfer.running){
+			try {
+				urlConnection = (HttpURLConnection) url.openConnection();
+				urlConnection.setRequestMethod("GET");
+				urlConnection.setDoOutput(true);
+				urlConnection.connect();
+				Log.d("WiFiDataTransfer", Environment.getExternalStorageDirectory().getPath()+"/result/");
+				SDCardRoot = new File(Environment.getExternalStorageDirectory().getPath()+"/result/");
+				destinationFile = new File(SDCardRoot,"sitemap.xml");
+				if(!destinationFile.exists()){
+					destinationFile.createNewFile();
+					destinationFile.canWrite();
+				}
+				fileOutput = new FileOutputStream(destinationFile);
+				inputStream = urlConnection.getInputStream();
+				byte[] buffer = new byte[1024];
+
+				int bufferLength = 0; 
+				while ( (bufferLength = inputStream.read(buffer)) > 0 ) 
+				{
+					fileOutput.write(buffer, 0, bufferLength);
+				}
+			} catch (Exception e) {
+				Log.e("WiFiDataTransfer", "Exception while getting data from url");
+				e.printStackTrace();
+			} finally {
+				try {
+					if(null != fileOutput){
+						fileOutput.flush();
+						fileOutput.close();
+					}
+					if(null != destinationFile){
+						destinationFile.delete();
+					}
+					if(null != inputStream){
+						inputStream.close();
+					}
+					if(null != urlConnection){
+						urlConnection.disconnect();
+						urlConnection = null;
+					}
+					
+				} catch (IOException e) {
+					Log.e("WiFiDataTransfer", "Exception cleaning up");
+					e.printStackTrace();
+				}
+				
+			}
+		}
+
 	}
 }
