e94956847c13e4025c1b1010cd20c38356df7465
Pekka Ekman <pekka.ekman@aalto.fi>
2014-01-25 21:34:54 +0200

    Move icon directory.
    When under src, it was unnecessarily included in the apk package file.

--- icon/build-icon.sh
+++ icon/build-icon.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+# This script builds png icons suitable for Android from an svg file.
+# The conversion is done using ImageMagick, which must be installed.
+
 SRC_DIR="`dirname \"$0\"`"
-DST_BASE_DIR="$SRC_DIR/../../res"
+DST_BASE_DIR="$SRC_DIR/../res"
 
 SRC="$SRC_DIR/icon.svg"
 DST_FILENAME="ic_launcher.png"
@@ -12,6 +15,8 @@ process () {
         -background transparent \
         "$SRC" \
         -gravity center -resize ${2}x${2} -extent ${2}x${2} \
+        +set date:create +set date:modify +set svg:base-uri \
+        -depth 8 \
         "$DST_BASE_DIR/drawable-${1}dpi/$DST_FILENAME"
 }
 
